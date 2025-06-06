4ef372e9fcecb0c953723a49e52be89f7cf45a2d
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-06-06 15:18:57 +0200

    Query

---
+++ repa_backend/routers/annotated_papers.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const { getDB } = require("../db"); 
+
+/**
+ * @swagger
+ * /annotated_papers:
+ *     get:
+ *         tags:
+ *             - Annotated papers
+ *         summary: Get list of anotated papers
+ *         responses:
+ *             200:
+ *                 description: List of annotated papers
+ */
+
+router.get("/", async (req, res) => {
+    try {
+        const db = getDB();
+        const annotatedPapers = await db.collection("annotated_papers")
+        .find({}, { projection: { _id: 0 } })
+        .toArray();
+        res.json(annotatedPapers);
+    } catch (err) {
+        console.error("Error fetching annotated papers:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+/**
+ * @swagger
+ * /annotated_papers/{corpus_id}:
+ *     get:
+ *         tags:
+ *             - Annotated papers
+ *         summary: Get annotated papers by corpusId
+ *         parameters:
+ *             - in: path
+ *               name: corpus_id
+ *               required: true
+ *               schema:
+ *                   type: string
+ *               description: The ID of the corpus annotated papers
+ *         responses:
+ *             200:
+ *                 description: Annotated papers found
+ *             404:
+ *                 description: Not found
+ */
+
+router.get("/:corpus_id", async (req, res) => {
+    try {
+        const db = getDB();
+        const corpusId = parseInt(req.params.corpus_id, 10);
+        const annotatedPaper = await db.collection("annotated_papers")
+        .findOne({ corpusid: corpusId }, { projection: { _id: 0 } });
+
+        if (annotatedPaper) {
+        res.json(annotatedPaper);
+        } else {
+        res.status(404).json({ error: "No annotated papers was found with the given corpus ID" });
+        }
+    } catch (err) {
+        console.error("Error fetching annotated paper:", err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+module.exports = router;
