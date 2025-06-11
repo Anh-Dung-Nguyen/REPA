1fa68a0c80627a389d95d04f370ecdb40b741dae
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-06-11 22:04:25 +0200

    Update App.js

--- repa_frontend/src/App.js
+++ repa_frontend/src/App.js
@@ -4,15 +4,21 @@ import axios from 'axios';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import MetricCard from './components/MetricCard';
+import FieldCard from './components/FieldCard';
 import ResearcherCard from './components/ResearcherCard';
-import { Users, BookOpen, BookIcon, LayoutDashboard, User2 } from 'lucide-react';
+import ResearcherDetail from './components/ResearcherDetail';
+import { Users, BookOpen, BookIcon, LayoutDashboard, User2, Paperclip } from 'lucide-react';
 
 function App() {
+  const [selectedResearcher, setSelectedResearcher] = useState(null);
+  const [selectedResearchTitle, setSelectedResearchTitle] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
   const [researcherStats, setResearcherStats] = useState({
     totalResearchers: 0,
     totalPapers: 0,
     totalAnnotatedPapers: 0,
+    totalSpecificTopics: 0,
   });
   const [searchResults, setSearchResults] = useState([]);
   const [currentTab, setCurrentTab] = useState("Overview");
@@ -22,6 +28,10 @@ function App() {
       setSearchResults([]);
         return;
     }
+
+    setLoading(true);
+    await new Promise(resolve => setTimeout(resolve, 500));
+
     try {
       const response = await axios.get(`http://localhost:8000/authors/search`, {
         params: { name: searchTerm }
@@ -30,15 +40,57 @@ function App() {
       setSearchResults(response.data.authors || []);
     } catch (error) {
       console.error("Error searching authors:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  const handleViewDetails = (researcher) => {
-    console.log("Researcher: ", researcher);
+  const handleViewDetails = async (researcher) => {
+    setSelectedResearcher(researcher);
+
+    try {
+      const res = await axios.get(`http://localhost:8000/authors_papers_annotations/${researcher.authorid}`);
+      
+      console.log("API Response:", res.data);
+
+      const papers = Array.isArray(res.data[0]?.papers)
+        ? res.data[0].papers.map(paper => ({title: paper.title, corpusid: paper.annotation.corpusid}))
+        : [];
+
+      console.log("Extracted paper titles:", papers);
+
+      setSelectedResearchTitle(papers);    
+    } catch (error) {
+      console.error("Error fetching titles", error);
+      setSelectedResearchTitle([]);
+    }
   };
+
+  const handleCloseDetails = () => {
+    setSelectedResearcher(null);
+  }
 
   const handleCompare = (researcher) => {
     console.log("Compare researcher: ", researcher.name);
+  };
+
+  const handleTitle = async (paper) => {
+    if (!paper?.corpusid) {
+      alert("No corpusid found for this paper.");
+      return;
+    }
+    try {
+      const res = await axios.get(`http://localhost:8000/papers_with_annotations/url/${paper.corpusid}`);
+      if (res.data?.url) {
+        const cleanUrl = res.data.url.replace(/^"|"$/g, '');
+        window.open(cleanUrl, '_blank');
+      } else {
+        alert("URL not found for this paper.");
+      }
+    } catch (error) {
+      console.error("Error fetching paper URL:", error);
+      alert("Failed to load paper URL.");
+    }
   };
 
   useEffect(() => {
@@ -47,10 +99,12 @@ function App() {
         const researcher = await axios.get('http://localhost:8000/authors/count');
         const paper = await axios.get('http://localhost:8000/papers/count');
         const annotated_paper = await axios.get('http://localhost:8000/annotated_papers/count');
+        const specific_topic = await axios.get('http://localhost:8000/specific_topics/count');
         setResearcherStats({
           totalResearchers: researcher.data.totalAuthors,
           totalPapers: paper.data.totalPapers,
           totalAnnotatedPapers: annotated_paper.data.totalAnnotatedPapers,
+          totalSpecificTopics: specific_topic.data.specific_topics,
         });
       } catch (error) {
         console.error("Error fetching stats: ", error);
@@ -81,6 +135,13 @@ function App() {
               <User2 size = {18}/>
               <strong>Researchers</strong>
             </button>
+            <button
+              onClick={() => setCurrentTab('Research Fields')}
+              className={`flex items-center gap-2 px-4 py-2 rounded mx-auto ${currentTab === 'Research Fields' ? 'text-blue-600 border-b-2 border-blue-600 bg-blue-50': 'text-gray-600 hover:text-gray-900 hover:bg-gray-50'}`}
+            >
+              <BookOpen size = {18}/>
+              <strong>Research Fields</strong>
+            </button>
           </div>
         </div>
       </div>
@@ -102,6 +163,11 @@ function App() {
               value = {researcherStats.totalAnnotatedPapers}
               icon = {BookIcon}
               color = "#8B5CF6"/>
+            <MetricCard 
+              title = "Total Specific Topics" 
+              value = {researcherStats.totalSpecificTopics}
+              icon = {Paperclip}
+              color = "#ffA500"/>
           </div>
         )}
             
@@ -113,7 +179,13 @@ function App() {
               onSearch = {handleSearch}
             />
 
-            {searchResults.length > 0 && (
+            {loading && (
+              <div className = 'flex justify-center mt-6'>
+                <div className="animate-spin rounded-full h-8 w-8 border-4 border-blue-500 border-t-transparent"></div>
+              </div>
+            )}
+
+            {!loading && searchResults.length > 0 && (
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
                 {searchResults.map((author) => (
                   <ResearcherCard
@@ -127,7 +199,22 @@ function App() {
             )}
           </>
         )}
+
+        {currentTab === "Research Fields" && (
+          <div className = 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6'>
+            <FieldCard />
+          </div>
+        )}
       </main>
+
+      {selectedResearcher && Array.isArray(selectedResearchTitle) && (
+        <ResearcherDetail
+          researcher={selectedResearcher}
+          researcherTitle={selectedResearchTitle}
+          onClose={handleCloseDetails}
+          handleTitle={handleTitle}
+        />
+      )}
     </div>      
   );
 }
