0e86a53da5cb2a76e4fd8654886301bcf439559d
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-06-26 14:58:26 +0200

    Small update

--- repa_frontend/src/components/TopResearchFieldChart.jsx
+++ repa_frontend/src/components/TopResearchFieldChart.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
-const TopResearchFieldsChart = ({ fieldsData }) => {
-  return (
-    <div className = "bg-white rounded-lg shadow-md p-6">
-      <h3 className = "text-lg font-semibold text-gray-900 mb-4">Top Research Fields</h3>
-      <ResponsiveContainer width = "100%" height = {500}>
-        <BarChart data = {fieldsData}>
-          <CartesianGrid strokeDasharray = "3 3" />
-          <XAxis dataKey = "name" angle = {-45} textAnchor = "end" height = {130} interval = {0} />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey = "researchers" fill = "#3B82F6" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
-  );
-};
+const TopResearchFieldsChart = ({ fieldsData, height }) => (
+  <div>
+    <h3 className="text-xl font-semibold mb-4 text-gray-800">Top 10 Research Fields by Researchers</h3>
+    <ResponsiveContainer width="100%" height={height}>
+      <BarChart
+        data={fieldsData}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <XAxis dataKey="name" angle={-45} textAnchor="end" interval={0} height={130} />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="researchers" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  </div>
+);
 
 export default TopResearchFieldsChart;
