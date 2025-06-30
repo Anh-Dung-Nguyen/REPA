2fee11fde325ada278af08d26d5def7d13a46e44
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-06-30 22:12:38 +0200

    Add link to return to the overview page in logo

--- repa_frontend/src/components/Header.jsx
+++ repa_frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => (
-    <header className = "bg-blue-600 text-white p-4 shadow-md rounded-b-lg">
-        <div className = "container mx-auto flex justify-between items-center">
-            <h1 className = "text-2xl font-bold">REPA Dashboard</h1>
+    <header className="bg-blue-600 text-white p-4 shadow-md rounded-b-lg">
+        <div className="container mx-auto flex justify-between items-center">
+            <Link to="/overview" className="text-2xl font-bold">
+                REPA Dashboard
+            </Link>
             <p>Research Performance Analyzer - Computer Science Researchers</p>
         </div>
     </header>
