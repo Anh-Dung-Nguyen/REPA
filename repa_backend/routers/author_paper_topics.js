8d74be594e3d84fb99aa1d6f1e161f55a030f593
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-07-05 21:06:47 +0200

    Add tab

--- repa_backend/routers/author_paper_topics.js
+++ repa_backend/routers/author_paper_topics.js
@@ -141,13 +141,13 @@ router.get("/author/:author_id", async (req, res) => {
         const db = getDB();
         const authorId = req.params.author_id;
         const topics = await db.collection("author_paper_topics")
-        .find({ authorId }, { projection: { _id: 0, paperId: 0 } })
-        .toArray();
+            .find({ authorId }, { projection: { _id: 0, paperId: 0 } })
+            .toArray();
 
         if (topics.length > 0) {
-        res.json(topics);
+            res.json(topics);
         } else {
-        res.status(404).json({ error: "No papers found for the given author ID" });
+            res.status(404).json({ error: "No papers found for the given author ID" });
         }
     } catch (err) {
         console.error("Error fetching author paper topics by author:", err);
