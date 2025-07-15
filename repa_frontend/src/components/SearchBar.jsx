e6738d186c7419331cbc2341e8697b58728ba239
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-07-15 21:45:46 +0200

    Update placeholder

--- repa_frontend/src/components/SearchBar.jsx
+++ repa_frontend/src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ const SearchBar = ({searchTerm, setSearchTerm, onSearch}) => (
                 <Search className = "absolute left-3 top-3 h-5 w-5 text-gray-400"/>
                 <input 
                     type = "text"
-                    placeholder = "Search researchers by name ..."
+                    placeholder = "Search by name ..."
                     className = "w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"                    value = {searchTerm}
                     onChange = {(e) => setSearchTerm(e.target.value)}
                     onKeyPress = {(e) => e.key === "Enter" && onSearch()}
