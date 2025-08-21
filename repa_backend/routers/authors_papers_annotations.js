afc4913201fa23eddd01210b61661a75efe0061b
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-08-21 17:34:45 +0200

    Update

--- repa_backend/routers/authors_papers_annotations.js
+++ repa_backend/routers/authors_papers_annotations.js
@@ -167,6 +167,99 @@ router.get("/author/:author_id", async (req, res) => {
 
 /**
  * @swagger
+ * /authors_papers_annotations/all_topics/author/{author_id}:
+ *   get:
+ *     tags:
+ *       - Author with papers and annotations
+ *     summary: Get list of papers with annotations and all topics (unfiltered) by authorId
+ *     parameters:
+ *       - in: path
+ *         name: author_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the author
+ *     responses:
+ *       200:
+ *         description: List of papers with all topics (unfiltered) by author
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal server error
+ */
+
+router.get("/all_topics/author/:author_id", async (req, res) => {
+    try {
+        const db = getDB();
+        const authorId = req.params.author_id;
+
+        const authorData = await db.collection("authors_papers_annotations")
+            .findOne(
+                { authorId },
+                { projection: { _id: 0, "papers.title": 1, "papers.annotation": 1, authorId: 1, name: 1 } }
+            );
+
+        if (!authorData) {
+            return res.status(404).json({ error: "No papers found for the given author ID" });
+        }
+
+        const axios = require("axios");
+
+        // ✅ Fetch ALL topics (unfiltered)
+        const topicsApiRes = await axios.get(
+            `http://localhost:8000/author_paper_topics/author/${authorId}`
+        );
+        const topicsList = topicsApiRes.data;
+
+        const corpusIdToTopics = new Map();
+        topicsList.forEach(item => {
+            corpusIdToTopics.set(item.corpusId, item.topics || []);
+        });
+
+        const enrichedPapers = await Promise.all(
+            authorData.papers.map(async (paper) => {
+                const paperDetails = await db.collection("papers_with_annotations")
+                    .findOne(
+                        { corpusid: Number(paper.annotation.corpusid) },
+                        { projection: { _id: 0, year: 1, referencecount: 1, citationcount: 1, influentialcitationcount: 1, venue: 1, abstract: 1, authors: 1 } }
+                    );
+
+                let numberOfCoAuthors = 0;
+                if (paperDetails && paperDetails.authors) {
+                    numberOfCoAuthors = paperDetails.authors.filter(a => a.authorId !== authorId).length;
+                }
+
+                return {
+                    ...paper,
+                    ...(paperDetails || {}),
+                    numberOfCoAuthors,
+                    allTopics: (() => {
+                        const corpusId = paper.annotation.corpusid;
+                        const topics = corpusIdToTopics.get(Number(corpusId));
+                        return topics || [];
+                    })()
+                };
+            })
+        );
+
+        res.json({
+            authorId: authorData.authorId,
+            name: authorData.name,
+            papers: enrichedPapers
+        });
+
+    } catch (err) {
+        console.error("Error fetching all topics for author:", err.message);
+        if (err.code === 'ECONNREFUSED' || err.response?.status) {
+            res.status(500).json({ error: "Error connecting to topics service" });
+        } else {
+            res.status(500).json({ error: "Internal server error" });
+        }
+    }
+});
+
+/**
+ * @swagger
  * /authors_papers_annotations/corpus/{corpus_id}:
  *   get:
  *     tags:
