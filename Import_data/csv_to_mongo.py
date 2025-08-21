91a298d14bda5b8c27f2cb585bdf338b7998ce46
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-08-21 17:34:03 +0200

    Update

--- Import_data/csv_to_mongo.py
+++ Import_data/csv_to_mongo.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from pymongo import MongoClient
 
-csv_file = '../Output/specific_topics_ranked.csv'
+csv_file = '../Output/all_topics_ranked.csv'
 df = pd.read_csv(csv_file)
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['research_db']
-collection = db['impact_one_topic']
+collection = db['impact_all_topic']
 
 data = df.to_dict(orient='records')
 collection.insert_many(data)
 
-print("Data inserted successfully into MongoDB collection 'impact_one_topic'.")
+print("Data inserted successfully into MongoDB collection 'impact_all_topic'.")
