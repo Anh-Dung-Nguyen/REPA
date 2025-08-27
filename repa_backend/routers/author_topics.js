41e70433df9071493b540651882365061dca772a
Anh-Dung Nguyen <nguyenanhdung2807@gmail.com>
2025-08-27 16:49:46 +0200

    Update

--- repa_backend/routers/author_topics.js
+++ repa_backend/routers/author_topics.js
@@ -29,6 +29,102 @@ router.get("/", async (req, res) => {
 
 /**
  * @swagger
+ * /author_topics/group_by_topic:
+ *   get:
+ *     tags:
+ *       - Author with topics
+ *     summary: Get paginated list of topics with associated authorIds
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           default: 60
+ *     responses:
+ *       200:
+ *         description: Paginated list of topics with their authors
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 topics:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       topic:
+ *                         type: string
+ *                       authorIds:
+ *                         type: array
+ *                         items:
+ *                           type: string
+ *                 totalCount:
+ *                   type: integer
+ *                 totalPages:
+ *                   type: integer
+ *                 currentPage:
+ *                   type: integer
+ *       500:
+ *         description: Internal server error
+ */
+
+router.get('/group_by_topic', async (req, res) => {
+    try {
+        const db = getDB();
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 60;
+        const skip = (page - 1) * limit;
+
+        const aggregationPipeline = [
+            { $unwind: "$topics" },
+            {
+                $group: {
+                    _id: "$topics",
+                    authorIds: { $addToSet: "$authorId" }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    topic: "$_id",
+                    authorIds: 1
+                }
+            },
+            { $sort: { topic: 1 } }, 
+            {
+                $facet: {
+                    metadata: [{ $count: "total" }],
+                    data: [{ $skip: skip }, { $limit: limit }]
+                }
+            }
+        ];
+
+        const results = await db.collection('author_topics').aggregate(aggregationPipeline).toArray();
+
+        const metadata = results[0].metadata[0] || { total: 0 };
+        const totalCount = metadata.total;
+        const totalPages = Math.ceil(totalCount / limit);
+
+        res.json({
+            topics: results[0].data,
+            totalCount,
+            totalPages,
+            currentPage: page
+        });
+    } catch (error) {
+        console.error('Error grouping authors by topic:', error.message);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+/**
+ * @swagger
  * /author_topics/group_by_topic/{topic}:
  *   get:
  *     tags:
